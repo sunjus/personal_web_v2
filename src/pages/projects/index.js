157ac2d64136a2f043d0f6f1d1360456002a1d8b
sunjus <sunju.shin1108@gmail.com>
2021-11-25 18:34:48 +0200

    Add sorting/ordering queries

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,24 +1,37 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.css"
 
-const Projects = () => {
+export default function Projects({ data }) {
+  console.log(data)
+
+  const projects = data.allMarkdownRemark.nodes
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've made</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
-export default Projects
 
 //export page query
-/*
+
 export const query = graphql`
   query ProjectsPage {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
         frontmatter {
           slug
@@ -30,4 +43,3 @@ export const query = graphql`
     }
   }
 `
-*/
