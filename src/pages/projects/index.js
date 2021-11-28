a6dbe0d6f962494d36e81b3a6ad364eeaf145ae3
sunjus <sunju.shin1108@gmail.com>
2021-11-28 20:13:06 +0200

    Fix error for image

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -9,23 +9,30 @@ export default function Projects({ data }) {
 
   const projects = data.projects.nodes
   const contact = data.contact.siteMetadata.contact
-  const image = getImage(projects.frontmatter.thumb)
-  console.log(image)
+  //const image = getImage(projects[0].frontmatter.thumb)
+  //console.log(image)
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've made</h3>
         <div className={styles.projects}>
-          {projects.map(project => (
-            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
-              <div>
-                <GatsbyImage image={image} />
-                <h3>{project.frontmatter.title}</h3>
-                <p>{project.frontmatter.stack}</p>
-              </div>
-            </Link>
-          ))}
+          {projects.map(project => {
+            const image = getImage(project.frontmatter.thumb)
+
+            return (
+              <Link
+                to={"/projects/" + project.frontmatter.slug}
+                key={project.id}
+              >
+                <div>
+                  <GatsbyImage image={image} alt={project.frontmatter.title} />
+                  <h3>{project.frontmatter.title}</h3>
+                  <p>{project.frontmatter.stack}</p>
+                </div>
+              </Link>
+            )
+          })}
         </div>
         <p>Like what you see? Email me at {contact} for a quote!</p>
       </div>
