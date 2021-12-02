a7389f1c2c0bd0fccd6c011d98a78c52997ae692
sunjus <sunju.shin1108@gmail.com>
2021-12-02 02:29:23 +0200

    Add query variables

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,23 +1,48 @@
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/project-details.module.css"
-//import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+  const image = getImage(featuredImg)
+
   return (
     <Layout>
       <div classNam={StyleSheet.details}>
-        <h2>title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
       </div>
       <div className={styles.featured}>
-        {/*<StaticImage
-          src="../images/banner.png"
-          alt="site banner"
-          placeholder="blurred"
-        />*/}
+        <GatsbyImage image={image} />
       </div>
-      {/*<div className={styles.html} dangerouslySetInnerHTML={}></div>*/}
+      <div
+        className={styles.html}
+        dangerouslySetInnerHTML={{ __html: html }}
+      ></div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectsDetail($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData(
+              blurredOptions: { width: 100 }
+              placeholder: BLURRED
+              transformOptions: { cropFocus: CENTER }
+            )
+          }
+        }
+      }
+    }
+  }
+`
